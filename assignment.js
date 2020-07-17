ab6bc41efd363996551723d8b04658b42662a57b
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-07-17 09:58:26 +0600

    added feetToMile

--- assignment.js
+++ assignment.js
@@ -1,4 +1,15 @@
 //feetToMile
+function feetToMile(feet){
+    if(feet < 0){
+        return 'Please enter positive value';
+    }
+    else {
+        var mile = feet / 5280;
+        var result = mile.toFixed(5);
+        return result;
+    }
+}
+
 //woodCalculator
 //brickCalculator
 //tinyFriends
