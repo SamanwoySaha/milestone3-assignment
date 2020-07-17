cecafb0837b2981985fff107081ea4b259aec6f6
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-07-17 10:20:03 +0600

    added woodCalculator

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 //feetToMile
-function feetToMile(feet){
-    if(feet < 0){
-        return 'Please enter positive value';
+function feetToMile(feet) {
+    if(feet < 0 || typeof(feet) == 'string'){
+        return 'Please enter the number of positive value';
     }
     else {
         var mile = feet / 5280;
@@ -11,5 +11,16 @@ function feetToMile(feet){
 }
 
 //woodCalculator
+function woodCalculator(chair, table, bed) {
+    if (chair < 0 || table < 0 || bed < 0 || typeof(chair) == 'string' || typeof(table) == 'string' || typeof(bed) == 'string'){
+        return 'Please enter the number of positive value';
+    }
+    else {
+        var totalWood = (chair * 1) + (table * 3) + (bed * 5);
+        return totalWood;
+    }
+}
+
 //brickCalculator
+
 //tinyFriends
